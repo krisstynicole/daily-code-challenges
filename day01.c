59072a9c86eae9e422e1dea6a03e72a32d678976
krisstynicole <66191765+krisstynicole@users.noreply.github.com>
2025-05-05 21:56:04 -0700

    Add files via upload

---
+++ day01.c
@@ -0,0 +1,15 @@
+// Integer Challenge //
+
+#include <stdio.h>
+int main () {
+
+    // Declare integer
+    int number;
+
+    // Assign the value
+    number = 50;
+
+    //print the value
+    printf("The value is: %d\n", number);
+    return 0;
+}
